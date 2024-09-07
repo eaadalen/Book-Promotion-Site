b060c8cf4c1dd2f863527a06a27a0fc7c93c42b9
eaadalen <eaadalen112@gmail.com>
2024-09-07 18:38:10 -0500

    Add boilerplate

---
+++ client/src/components/welcome-view/welcome-view.jsx
@@ -0,0 +1,10 @@
+import "./welcome-view.css"
+import { Link } from "react-router-dom";
+
+export const WelcomeView = () => {
+  return (
+    <div className='select-service'>
+      Test
+    </div>
+  )
+}
